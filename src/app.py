ea11e676e307c0a511121855ee07202826605194
aarush-narang <aarushnarang@gmail.com>
2022-03-06 22:50:54 -0800

    added loading of previous chat logs and chat functionality

--- src/app.py
+++ src/app.py
@@ -15,16 +15,38 @@ app.register_blueprint(main_router.main_router, url_prefix='/')
 csrf = CSRFProtect()
 csrf.init_app(app)
 
-
-def log_socket(message):
+def log_socket(message=''):
     with open(f'{os.getcwd()}/logs/socket.log', 'a') as f:
         f.write(f'{datetime.now()} - {message}\n')
 
+def log_chat(msg='', user='', ip=''):
+    with open(f'{os.getcwd()}/logs/chat.log', 'a') as f:
+        f.write(f'[{datetime.now()}|{user}|{ip}]::{msg}\n')
+
+def getAllChatLogs():
+    msgs = []
+    users = []
+    with open(f'{os.getcwd()}/logs/chat.log', 'r') as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        for line in lines:
+            split = line.split('::')
+
+            msg = split[1]
+            user = split[0].split('|')[1]
+
+            msgs.append(msg)
+            users.append(user)
+    
+    return msgs, users
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 @socketio.on('connect')
 def handle_connect():
     log_socket(f'[CLIENT CONNECTED]')
+
+    chatLogs = getAllChatLogs()
+    emit('connect', { 'msgs': chatLogs[0], 'users': chatLogs[1]}) # on connect, send all messages
 
 @socketio.on('disconnect')
 def handle_disconnect():
@@ -35,10 +57,10 @@ def handle_disconnect():
 def handle_message(msg):
     log_socket(f'[MESSAGE] {msg}')
 
-@socketio.on('json')
-def handle_json(json):
-    print(f'[JSON MESSAGE] {str(json)}')
-
+@socketio.event
+def user_message(msg, user, ip):
+    log_chat(msg, user, ip)
+    emit('user_message', { 'msg': msg, 'user': user }, broadcast=True)
 
 
 if __name__ == '__main__':
--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,23 +1,70 @@
-const input = document.getElementById('input-text')
-const submit = document.getElementById('input-submit')
-const inputsList = document.getElementById('inputs')
-const disconnect = document.getElementById('disconnect')
 const connect = document.getElementById('connect')
+const disconnect = document.getElementById('disconnect')
 
+const msgInput = document.getElementById('input-text')
+const msgSubmit = document.getElementById('input-submit')
+const userInput = document.getElementById('input-user')
+const userSubmit = document.getElementById('user-submit')
+
+const inputsList = document.getElementById('inputs-list')
+
+
+
+// connect and disconnect
 const socket = io.connect('https://' + window.location.hostname + ':5000');
+socket.on('connect', function(msg) {
+    if(!msg) return
+    const msgs = msg.msgs
+    const users = msg.users
 
-disconnect.addEventListener('click', () => {
-    socket.disconnect()
-})
-
-connect.addEventListener('click', () => {
-    socket.connect()
-})
-
-submit.addEventListener('click', () => {
-    const inputValue = input.value
-    if (inputValue) {
-        socket.emit('message', inputValue)
-        input.value = ''
+    for (let i = 0; i < msgs.length; i++) {
+        const msg = msgs[i]
+        const user = users[i]
+        const li = document.createElement('li')
+        li.innerHTML = `${user}: ${msg}`
+        inputsList.appendChild(li)
     }
+})
+
+if (window.closed) {
+    socket.disconnect()
+}
+
+
+userSubmit.addEventListener('click', () => {
+    if(userInput.value.length < 1) return
+    if(userInput.value.length > 20) return alert('Username must be less than 20 characters')
+    userInput.style.cursor = 'not-allowed'
+    userSubmit.style.cursor = 'not-allowed'
+    userInput.toggleAttribute('disabled', true)
+
+    msgInput.style.cursor = 'auto'
+    msgSubmit.style.cursor = 'auto'
+    msgInput.toggleAttribute('disabled', false)
+    msgInput.focus()
+    return
+})
+// send message to server
+msgSubmit.addEventListener('click', () => {
+    if (!userInput.value || userInput.style.cursor !== 'not-allowed') {
+        msgInput.style.cursor = 'not-allowed'
+        msgSubmit.style.cursor = 'not-allowed'
+        msgInput.toggleAttribute('disabled', true)
+        return
+    }
+    if (msgInput.value.length > 2000) return alert('Message must be less than 2000 characters')
+    
+    const inputValue = msgInput.value
+    if (inputValue) {
+        socket.emit('user_message', inputValue, userInput.value, '0.0.0.0')
+        msgInput.value = ''
+    }
+    msgInput.focus()
+})
+
+// recieve message from server
+socket.on('user_message', (message) => {
+    const li = document.createElement('li')
+    li.innerHTML = `${message.user}: ${message.msg}`
+    inputsList.appendChild(li)
 })
